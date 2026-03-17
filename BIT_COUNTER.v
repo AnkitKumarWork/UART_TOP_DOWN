704b009c8ab33bfe234bade6c9ba663eb2237c07
AnkitKumar <ankitkumar.work121@gmail.com>
2026-03-17 22:55:00 +0530

    Add files via upload

--- BIT_COUNTER.v
+++ BIT_COUNTER.v
@@ -3,18 +3,28 @@ module BIT_COUNTER(
     input clk,
     input rst,
     input bit_enb,
-    output [3:0] bit_index
+    output RX_CMP
 );
 
 reg [3:0] count_value;
+reg rx_cmp_reg;
 
 always@(posedge clk or posedge rst)
 begin
-  if(rst )
+    if(rst)
         count_value <= 0;
-    else
-        if(bit_enb)
-            count_value <= count_value + 1;
+    else if(bit_enb)
+        count_value <= count_value + 1;
 end
-assign bit_index = count_value;
+
+always@(posedge clk or posedge rst)
+begin
+    if(rst)
+        rx_cmp_reg <= 0;
+    else
+        rx_cmp_reg <= (count_value == 4'd9);
+end
+
+assign RX_CMP = rx_cmp_reg;
+
 endmodule
